47a5ba2abadd6c2f5b8c2934485de142a968a1b3
PauloAlbes8039 <pj38alves@gmail.com>
2021-07-06 20:39:12 -0300

    Implementação inicial do contexto do banco de dados

---
+++ src/GameCatalogue.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using GameCatalogue.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameCatalogue.Infra.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {}
+
+        public DbSet<Platform> Platforms { get; set; }
+        public DbSet<Game> Games { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}

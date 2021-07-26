a68e0048bffab595bf3539a9b3cca62da658d518
unknown <pj38alves@gmail.com>
2021-07-26 16:11:12 -0300

    Definindo mapeamento e implementações dos Handlers e Queries

---
+++ src/GameCatalogue.Application/Mappings/DTOCommandMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using GameCatalogue.Application.DTOs;
+using GameCatalogue.Application.Games.Commands;
+
+namespace GameCatalogue.Application.Mappings
+{
+    public class DTOCommandMappingProfile : Profile
+    {
+        public DTOCommandMappingProfile()
+        {
+            CreateMap<GameDTO, GameCreateCommand>();
+            CreateMap<GameDTO, GameUpdateCommand>();
+        }
+    }
+}
--- src/GameCatalogue.Infra.IoC/DependencyInjection.cs
+++ src/GameCatalogue.Infra.IoC/DependencyInjection.cs
@@ -4,9 +4,11 @@ using GameCatalogue.Application.Services;
 using GameCatalogue.Domain.Interfaces;
 using GameCatalogue.Infra.Data.Context;
 using GameCatalogue.Infra.Data.Repositories;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace GameCatalogue.Infra.IoC
 {
@@ -24,6 +26,9 @@ namespace GameCatalogue.Infra.IoC
             services.AddScoped<IGameService, GameService>();
             services.AddScoped<IPlatformService, PlatformService>();
             services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+            var myhandlers = AppDomain.CurrentDomain.Load("GameCatalogue.Application");
+            services.AddMediatR(myhandlers);
 
             return services;
         }

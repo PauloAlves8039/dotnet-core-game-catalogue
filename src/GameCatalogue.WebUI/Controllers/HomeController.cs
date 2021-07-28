af9ec08fd56470b952e3dd44963a68dffa7a3843
unknown <pj38alves@gmail.com>
2021-07-28 14:02:34 -0300

    Adicionando o HomeController e definindo roteamento das Views Index

---
+++ src/GameCatalogue.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GameCatalogue.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
--- src/GameCatalogue.WebUI/Startup.cs
+++ src/GameCatalogue.WebUI/Startup.cs
@@ -49,7 +49,7 @@ namespace GameCatalogue.WebUI
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Games}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }

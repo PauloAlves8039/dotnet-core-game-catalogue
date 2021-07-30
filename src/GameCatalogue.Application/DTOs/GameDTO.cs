757abdba2f3cc5b62233bda286e9961a88bd68da
unknown <pj38alves@gmail.com>
2021-07-30 15:35:34 -0300

    Atualizando GameDTO e View Create

--- src/GameCatalogue.Application/DTOs/GameDTO.cs
+++ src/GameCatalogue.Application/DTOs/GameDTO.cs
@@ -12,27 +12,27 @@ namespace GameCatalogue.Application.DTOs
         [MinLength(3)]
         [MaxLength(100)]
         [DisplayName("Name")]
-        public string Name { get; private set; }
+        public string Name { get; set; }
 
         [Required(ErrorMessage = "The Producer is Required")]
         [MinLength(5)]
         [MaxLength(50)]
         [DisplayName("Producer")]
-        public string Producer { get; private set; }
+        public string Producer { get; set; }
 
         [Required(ErrorMessage = "The Gender is Required")]
         [MinLength(5)]
         [MaxLength(100)]
         [DisplayName("Gender")]
-        public string Gender { get; private set; }
+        public string Gender { get; set; }
 
         [Required(ErrorMessage = "The Quantity is Required")]
         [DisplayName("Quantity")]
-        public int Quantity { get; private set; }
+        public int Quantity { get; set; }
 
         [MaxLength(250)]
         [DisplayName("Game Image")]
-        public string Image { get; private set; }
+        public string Image { get; set; }
 
         [DisplayName("Platforms")]
         public int PlatformId { get; set; }

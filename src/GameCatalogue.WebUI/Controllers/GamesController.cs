9ead8dd7c94557b0c16d2e342162cec7333d72d3
unknown <pj38alves@gmail.com>
2021-07-30 12:16:37 -0300

    Implementação inicial da View Create para Game

--- src/GameCatalogue.WebUI/Controllers/GamesController.cs
+++ src/GameCatalogue.WebUI/Controllers/GamesController.cs
@@ -1,5 +1,7 @@
-﻿using GameCatalogue.Application.Interfaces;
+﻿using GameCatalogue.Application.DTOs;
+using GameCatalogue.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,17 +12,37 @@ namespace GameCatalogue.WebUI.Controllers
     public class GamesController : Controller
     {
         private readonly IGameService _gameService;
+        private readonly IPlatformService _platformService;
 
-        public GamesController(IGameService gameService)
+        public GamesController(IGameService gameService, IPlatformService platformService)
         {
             _gameService = gameService;
+            _platformService = platformService;
         }
-        
+
         [HttpGet]
         public async Task<IActionResult> Index()
         {
             var games = await _gameService.GetGames();
             return View(games);
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Create() 
+        {
+            ViewBag.PlatformId = new SelectList(await _platformService.GetPlatforms(), "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(GameDTO gameDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                await _gameService.Add(gameDTO);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(gameDTO);
         }
     }
 }

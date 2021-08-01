12ebe11dd0bc7d215c425944ba7f99a4975ce431
unknown <pj38alves@gmail.com>
2021-08-01 14:29:55 -0300

    Implementando as Views Delete e Details para Game

--- src/GameCatalogue.WebUI/Controllers/GamesController.cs
+++ src/GameCatalogue.WebUI/Controllers/GamesController.cs
@@ -1,10 +1,9 @@
 ﻿using GameCatalogue.Application.DTOs;
 using GameCatalogue.Application.Interfaces;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace GameCatalogue.WebUI.Controllers
@@ -13,11 +12,13 @@ namespace GameCatalogue.WebUI.Controllers
     {
         private readonly IGameService _gameService;
         private readonly IPlatformService _platformService;
+        private readonly IWebHostEnvironment _enviroment;
 
-        public GamesController(IGameService gameService, IPlatformService platformService)
+        public GamesController(IGameService gameService, IPlatformService platformService, IWebHostEnvironment environment)
         {
             _gameService = gameService;
             _platformService = platformService;
+            _enviroment = environment;
         }
 
         [HttpGet]
@@ -49,15 +50,11 @@ namespace GameCatalogue.WebUI.Controllers
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null) return NotFound();
-
             var gameDto = await _gameService.GetById(id);
-
             if (gameDto == null) return NotFound();
-
             var platforms = await _platformService.GetPlatforms();
 
             ViewBag.PlatformId = new SelectList(platforms, "Id", "Name", gameDto.PlatformId);
-
             return View(gameDto);
         }
 
@@ -71,6 +68,35 @@ namespace GameCatalogue.WebUI.Controllers
             }
 
             return View(gameDTO);
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Delete(int? id) 
+        {
+            if (id == null) return NotFound();
+            var gameDto = await _gameService.GetById(id);
+            if (gameDto == null) return NotFound();
+            return View(gameDto);
+        }
+
+        [HttpPost(), ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id) 
+        {
+            await _gameService.Remove(id);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Details(int? id) 
+        {
+            if (id == null) return NotFound();
+            var gameDto = await _gameService.GetById(id);
+            if (gameDto == null) return NotFound();
+
+            var wwwroot = _enviroment.WebRootPath;
+            var image = Path.Combine(wwwroot, "images\\" + gameDto.Image);
+            var exists = System.IO.File.Exists(image);
+            ViewBag.ImageExist = exists;
+            return View(gameDto);
         }
     }
 }

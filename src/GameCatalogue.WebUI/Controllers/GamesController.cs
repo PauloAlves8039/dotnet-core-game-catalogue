50c715978fd2d6de4a9e836e5c02117b959f42ca
unknown <pj38alves@gmail.com>
2021-07-31 21:11:28 -0300

    Implementando a View Edit para Game

--- src/GameCatalogue.WebUI/Controllers/GamesController.cs
+++ src/GameCatalogue.WebUI/Controllers/GamesController.cs
@@ -44,5 +44,33 @@ namespace GameCatalogue.WebUI.Controllers
             }
             return View(gameDTO);
         }
+
+        [HttpGet()]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var gameDto = await _gameService.GetById(id);
+
+            if (gameDto == null) return NotFound();
+
+            var platforms = await _platformService.GetPlatforms();
+
+            ViewBag.PlatformId = new SelectList(platforms, "Id", "Name", gameDto.PlatformId);
+
+            return View(gameDto);
+        }
+
+        [HttpPost()]
+        public async Task<IActionResult> Edit(GameDTO gameDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                await _gameService.Update(gameDTO);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(gameDTO);
+        }
     }
 }
